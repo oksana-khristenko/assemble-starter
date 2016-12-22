be9608080b124973f3ac2922ebd424b1eade6289
Oksana Khristenko <oksana.khristenko@gmail.com>
2016-12-22 22:35:41 +0000

    Add copyright handlebars helper

---
+++ app/helpers/helper-copyright.js
@@ -0,0 +1,25 @@
+var copyright = require('./modules/copyright');
+var config = require('./modules/config');
+
+module.exports.register = function(Handlebars, options) {
+
+    options = options || {};
+
+    var helpers = {
+
+        helper_copyright: function () {
+            var date = new Date(),
+                currentYear = date.getFullYear(),
+                startYear = Number(config.getStartYear(this));
+
+            return copyright.getText(startYear, currentYear);
+        }
+
+    };
+
+    for (var helper in helpers) {
+        if (helpers.hasOwnProperty(helper)) {
+            Handlebars.registerHelper(helper, helpers[helper]);
+        }
+    }
+}
