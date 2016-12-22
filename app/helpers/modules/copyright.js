d5312a70072866a3b7b2aab939d8a4078ff9a423
Oksana Khristenko <oksana.khristenko@gmail.com>
2016-12-22 22:18:08 +0000

    Refactor copyright module

--- app/helpers/modules/copyright.js
+++ app/helpers/modules/copyright.js
@@ -1,4 +1,4 @@
-exports.getCopyright = function(startYear, currentYear) {
+function validate(startYear, currentYear) {
     if (!Number.isInteger(startYear)) {
         throw new TypeError('startYear must be an integer');
     }
@@ -10,6 +10,9 @@ exports.getCopyright = function(startYear, currentYear) {
     if (startYear > currentYear) {
         throw new RangeError('startYear must be equal to or less than currentYear');
     }
+}
 
+exports.getCopyright = function(startYear, currentYear) {
+    validate(startYear, currentYear);
     return (startYear === currentYear) ? `${currentYear}` : `${startYear} - ${currentYear}`;
 };
