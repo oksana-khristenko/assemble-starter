5d896238acd4db9c473b588d732e34e416b7d197
Oksana Khristenko <oksana.khristenko@gmail.com>
2016-12-28 21:32:10 +0000

    Tidy up pageType tests

--- app/tests/pageType.test.js
+++ app/tests/pageType.test.js
@@ -1,7 +1,8 @@
-"use strict";
-var chai = require("chai");
-var sinon = require("sinon");
-var sinonChai = require("sinon-chai");
+'use strict';
+
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
 var expect = chai.expect;
 chai.use(sinonChai);
 
