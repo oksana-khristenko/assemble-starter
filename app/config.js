d881fbe093d27a333b56a350cb8ebddf34f23b5d
Oksana Khristenko <oksana.khristenko@gmail.com>
2016-12-28 17:35:42 +0000

    Add more social accounts to config

--- app/config.js
+++ app/config.js
@@ -7,6 +7,10 @@ module.exports = {
     twitterUserName: 'Twitter User Name', // e.g. @twitter
     twitterPageUrl: 'Twitter Page Url', // e.g. https://twitter.com/twitter
     facebookPageUrl: 'Facebook Page Url', // e.g. https://facebook.com/twitter,
+    pintrestPageUrl: 'Pintrest Page Url', // e.g. https://www.pinterest.com/dan330
+    googleplusPageUrl: 'Google Plus Page Url', // e.g. https://facebook.com/twitter,
+    linkedinPageUrl: 'Linkedin Page Url', // e.g. https://www.linkedin.com/in/bryankratz,
+    instagramPageUrl: 'Instagram Page Url', // e.g. https://www.instagram.com/teslamotors/,
     googleAnalyticsId: 'Google Analytics Id', // e.g. UA-000000-01
     googleAnalyticsDomain: 'Google Analytics Domain', // e.g. twitter.com
     enableGoogleAnalytics: true // e.g. false
