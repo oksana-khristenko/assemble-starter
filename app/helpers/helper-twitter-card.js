8cea3b0bc9d0ce2bb4756e9991c3f1911474c030
Oksana Khristenko <oksana.khristenko@gmail.com>
2016-12-29 15:40:48 +0000

    Add handlebars helper twitter card

---
+++ app/helpers/helper-twitter-card.js
@@ -0,0 +1,22 @@
+var TwitterCard = require('./modules/TwitterCard');
+var configObj = require('../config.js');
+
+module.exports.register = function(Handlebars, options) {
+
+    options = options || {};
+
+    var helpers = {
+
+        helper_twitter_card: function(options) {
+            var twitterCard = new TwitterCard(configObj, this.page);
+            return options.fn(twitterCard.get());
+        }
+
+    };
+
+    for (var helper in helpers) {
+        if (helpers.hasOwnProperty(helper)) {
+            Handlebars.registerHelper(helper, helpers[helper]);
+        }
+    }
+}
