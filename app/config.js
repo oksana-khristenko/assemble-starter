4e3717bd5ccf789efc818da5ce89352e13e48a4a
Oksana Khristenko <oksana.khristenko@gmail.com>
2017-01-06 13:13:34 +0000

    Update config

--- app/config.js
+++ app/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    projectStartYear: '2015', // e.g. 2018
-    projectName: 'Project Name', // e.g. Twitter
+    startYear: '2015', // e.g. 2018 - used in footer copyright
+
+    siteName: 'Project Name', // e.g. Twitter
     siteUrl: 'Site Url', // e.g. https://twitter.com/
 
     twitterCardEnabled: true, // e.g. true
@@ -8,11 +9,16 @@ module.exports = {
     twitterUserName: 'Twitter User Name', // e.g. @twitter
     twitterPageUrl: 'Twitter Page Url', // e.g. https://twitter.com/twitter
 
+    facebookAppId: '',
+
+    openGraphMarkupEnabled: false, // e.g. false
+
     facebookPageUrl: 'Facebook Page Url', // e.g. https://facebook.com/twitter,
     pintrestPageUrl: 'Pintrest Page Url', // e.g. https://www.pinterest.com/dan330
     googleplusPageUrl: 'Google Plus Page Url', // e.g. https://facebook.com/twitter,
     linkedinPageUrl: 'Linkedin Page Url', // e.g. https://www.linkedin.com/in/bryankratz,
     instagramPageUrl: 'Instagram Page Url', // e.g. https://www.instagram.com/teslamotors/,
+
     googleAnalyticsId: 'Google Analytics Id', // e.g. UA-000000-01
     googleAnalyticsDomain: 'Google Analytics Domain', // e.g. twitter.com
     googleAnalyticsEnabled: false // e.g. false
