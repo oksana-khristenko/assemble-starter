27c448c3672b174df3dcfa3cff1e29f85983b2b3
Oksana Khristenko <oksana.khristenko@gmail.com>
2017-01-07 07:14:35 +0000

    Validate arguments passed from template to helper_config

--- app/helpers/register-helpers/helper-config.js
+++ app/helpers/register-helpers/helper-config.js
@@ -3,9 +3,23 @@ var ConfigBuilder = require('../builders/ConfigBuilder');
 module.exports.register = function(Handlebars, options) {
     options = options || {};
 
+    var helperName = 'helper_config';
+
+    function validateArguments(arguments) {
+        if (arguments.length < 2) {
+            throw new ReferenceError(`${helperName}: config key must be passed from template; nothing was passed`);
+        }
+        if (arguments.length === 2 && typeof arguments[0] != 'string') {
+            var argType = typeof arguments[0];
+            throw new TypeError(`${helperName}: config key must be a string when passed from template; ${argType} was passed`);
+        }
+    }
+
     var helpers = {
-        helper_config: function(key) {
-            var config = new ConfigBuilder().build();
+        [helperName]: function(key, options) {
+            validateArguments(arguments);
+
+            var config = new ConfigBuilder({helperName: helperName}).build();
             return config.get(key);
         }
     };
