017233421e5ab379b98820c75c589503cab59898
Oksana Khristenko <oksana.khristenko@gmail.com>
2017-01-07 19:42:30 +0000

    Fix indentation

--- app/helpers/builders/PagePropertyBuilder.js
+++ app/helpers/builders/PagePropertyBuilder.js
@@ -6,16 +6,16 @@ var PagePropertyValidator = require('../validators/PagePropertyValidator');
 class PagePropertyBuilder {
 
     constructor(obj) {
-    	this.helperName = obj && obj.helperName;
+        this.helperName = obj && obj.helperName;
         this.page = obj && obj.page;
         this.validator = (obj && obj.validator) || new PagePropertyValidator({helperName: this.helperName});
     }
 
     build() {
         return new PageProperty({
-        	page: this.page,
-        	validator: this.validator,
-        	helperName: this.helperName
+            page: this.page,
+            validator: this.validator,
+            helperName: this.helperName
         });
     }
 
